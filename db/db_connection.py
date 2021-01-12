7ac93372e244e0b16a271bb3646d15437bb29220
Mikheltodd <68166999+Mikheltodd@users.noreply.github.com>
2021-01-12 18:33:39 -0500

    Heroku DB Data

--- db/db_connection.py
+++ db/db_connection.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Creación del Motor
-db_url = "postgresql://postgres:root@localhost:5432/BarberShop"
+db_url = "postgres://jomlyhfohmirsf:d6b3cca5255c9dbd1fc549cc7ee8183da74ee40818576ea5d315b3d70053587f@ec2-35-168-77-215.compute-1.amazonaws.com:5432/dclfag75opedrp"
 engine = create_engine(db_url)
 
 # Creación de Sesión y Dependencias
